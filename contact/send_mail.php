1c9f177374c347d61bd265564907a1fd57a6d90e
hee2222 <98146685+hee2222@users.noreply.github.com>
2023-01-03 16:46:57 +0900

    Add files via upload

--- contact/send_mail.php
+++ contact/send_mail.php
@@ -1,6 +1,6 @@
 <?php
 
-$object = $_POST["contact-form1"];
+$object = $_GET["contact-form1"];
 $membersfile = $_FILES['members-file'];
 $contactfile = $_FILES['contact-file'];
 
@@ -20,20 +20,20 @@ $mailbody .= "Content-Type: text/html; charset=\"ks_c_5601-1987\"\r\n";
 //암호화 방식을 알려줌
 $mailbody .= "Content-Transfer-Encoding: base64\r\n\r\n";
 
-$form2_result = join(", ", $_POST["contact-form2"]);
-$form3_result = join(", ", $_POST["contact-form3"]);
-$form4_result = join(", ", $_POST["contact-form4"]);
+$form2_result = join(", ", $_GET["contact-form2"]);
+$form3_result = join(", ", $_GET["contact-form3"]);
+$form4_result = join(", ", $_GET["contact-form4"]);
 
 
 //이메일 내용을 암호화 해서 추가
 if ($object == 'member') {
-	$title = $_POST["members-info-name"] . '님 멤버 지원';
-	$msg = '지원 포지션 : ' . $_POST["members-form2"] . '<br><br>';
+	$title = $_GET["members-info-name"] . '님 멤버 지원';
+	$msg = '지원 포지션 : ' . $_GET["members-form2"] . '<br><br>';
 	$msg .= '기본 정보 <br>';
-	$msg .= '이름 : ' . $_POST["members-info-name"] . '<br>';
-	$msg .= '연락처 : ' . $_POST["members-info-tel"] . '<br>';
-	$msg .= '이메일 : ' . $_POST["members-info-mail"] . '<br>';
-	$msg .= '간단 소개 : ' . $_POST["contact-member-form4"] . '<br>';
+	$msg .= '이름 : ' . $_GET["members-info-name"] . '<br>';
+	$msg .= '연락처 : ' . $_GET["members-info-tel"] . '<br>';
+	$msg .= '이메일 : ' . $_GET["members-info-mail"] . '<br>';
+	$msg .= '간단 소개 : ' . $_GET["contact-member-form4"] . '<br>';
 
 	$mailbody .= base64_encode(nl2br($msg)) . "\r\n\r\n";
 	// 내용 구분 시작
@@ -57,23 +57,23 @@ if ($object == 'member') {
 		}
 	}
 } else if ($object == 'project') {
-	$title = $_POST["contact-info-com"] . ' / ' . $_POST["contact-info-name"] . ' 프로젝트 의뢰';
+	$title = $_GET["contact-info-com"] . ' / ' . $_GET["contact-info-name"] . ' 프로젝트 의뢰';
 	$msg = '프로젝트 분류 : ' .  $form2_result . '<br>';
-	if (in_array("video", $_POST["contact-form2"])) {
+	if (in_array("video", $_GET["contact-form2"])) {
 		$msg .= '영상 종류 : ' . $form3_result . '<br>';
 	}
-	if (in_array("design", $_POST["contact-form2"])) {
+	if (in_array("design", $_GET["contact-form2"])) {
 		$msg .= '디자인 종류 : ' . $form4_result  . '<br>';
 	}
 
 	$msg .= '예상일정 <br>';
-	$msg .=  $_POST["start-year"] . '년 ' . $_POST["start-month"] . '월 ' . $_POST["start-day"] . '부터 ' . $_POST["start-year"] . '년 ' . $_POST["start-month"] . '월 ' . $_POST["start-day"] . ' 까지<br>';
-	$msg .=  '예산 : ' . $_POST["contact-form6"] . '<br><br>';
+	$msg .=  $_GET["start-year"] . '년 ' . $_GET["start-month"] . '월 ' . $_GET["start-day"] . '부터 ' . $_GET["start-year"] . '년 ' . $_GET["start-month"] . '월 ' . $_GET["start-day"] . ' 까지<br>';
+	$msg .=  '예산 : ' . $_GET["contact-form6"] . '<br><br>';
 	$msg .= '기본 정보 <br>';
-	$msg .= '회사명 / 이름 : ' . $_POST["contact-info-com"] . ' / ' . $_POST["contact-info-name"] . '<br>';
-	$msg .= '연락처 : ' . $_POST["contact-info-tel"] . '<br>';
-	$msg .= '이메일 : ' . $_POST["contact-info-mail"] . ' <br>';
-	$msg .= '간단 소개 : ' . $_POST["contact-form8"] . '<br>';
+	$msg .= '회사명 / 이름 : ' . $_GET["contact-info-com"] . ' / ' . $_GET["contact-info-name"] . '<br>';
+	$msg .= '연락처 : ' . $_GET["contact-info-tel"] . '<br>';
+	$msg .= '이메일 : ' . $_GET["contact-info-mail"] . ' <br>';
+	$msg .= '간단 소개 : ' . $_GET["contact-form8"] . '<br>';
 
 	$mailbody .= base64_encode(nl2br($msg)) . "\r\n\r\n";
 	// 내용 구분 시작
